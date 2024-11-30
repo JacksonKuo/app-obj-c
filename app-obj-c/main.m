90bbf5fb0e79597150acfc0b0db20ac697caadb3
JacksonKuo <JacksonKuo@users.noreply.github.com>
2024-11-30 04:21:21 -0600

    Initial Commit

---
+++ app-obj-c/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  app-obj-c
+//
+//  Created by JacksonKuo on 11/30/24.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
