85b2f26a878317ca5afceb111b4db02a6c8e5241
JacksonKuo <JacksonKuo@users.noreply.github.com>
2024-12-01 06:48:59 -0600

    initial commit

---
+++ app-obj-c/HelloWorld.h
@@ -0,0 +1,14 @@
+//
+//  helloworld.h
+//  app-obj-c
+//
+//  Created by JacksonKuo on 11/30/24.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HelloWorld : NSObject
+
+- (void)sayHello;
+
+@end
